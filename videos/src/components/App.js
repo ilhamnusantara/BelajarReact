defcc3c8e690dd2349a48b6da5a27b5668698515
ilhamnusantara <ilhamsdr@gmail.com>
2019-07-28 13:42:16 +0700

    Update

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import SearchBar from './SearchBar';
 import youtube from '../apis/youtube';
 import VideoList from './VideoList';
+import VideoDetail from './VideoDetail';
 
 class App extends React.Component {
     state = { videos: [], selectedVideo: null };
@@ -17,14 +18,23 @@ class App extends React.Component {
     };
 
     onVideoSelect = video => {
-        this.setState({ selectedVideo: video });
+        this.setState({ selectedVideo: video }); // play youtube layar utama
     };
 
     render(){
         return (
             <div className="ui container">
-            <SearchBar onFormSubmit={this.onTermSubmit}/>
-            <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos}/>
+                <SearchBar onFormSubmit={this.onTermSubmit}/>
+                <div className="ui grid">
+                    <div className="ui row">
+                        <div className="eleven wide column">
+                        <VideoDetail video={this.state.selectedVideo}/>
+                        </div>
+                        <div className="five wide column">
+                        <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos}/>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
